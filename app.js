35c889fd94e4217a8b19fd3aa5c24ae8b1088787
João Victor Mota Machado <jvmm676@gmail.com>
2023-08-29 20:27:58 -0300

    style: formatação das linhas no app.js

--- app.js
+++ app.js
@@ -17,9 +17,11 @@ class PropriedadesLista {
 };
 
 const listaPropriedades = new PropriedadesLista();
+
 function recebeOrdenaPropriedade (){
     while(true){
         const entradaUsuario = rl.question(chalk.green("Insira as propriedades desejadas (ou SAIR para encerrar): "));
+        
         if(entradaUsuario.trim() === ''){
             console.log(chalk.redBright("\nInsira uma propriedade válida (ou SAIR para encerrar)!\n"));        
         } else if (entradaUsuario.toUpperCase() != "SAIR"){
@@ -30,10 +32,13 @@ function recebeOrdenaPropriedade (){
     };
     try{
         listaPropriedades.ordernarElementos();
+        
         console.log(chalk.magenta("Propriedades ordenadas de A-Z: \n---"));
+        
         listaPropriedades.mostrarElementos().forEach(elemento => {
             console.log(chalk.blueBright(elemento));
         });
+        
         console.log(chalk.magenta("---"));
     }catch(error){
         console.error("Ocorreu um erro ao executar a função: ", error.message);
